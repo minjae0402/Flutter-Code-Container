d334dcd8a21e4a69ec814ac774f6e26643fc514d
minjae0402 <kangjack0402@gmail.com>
2024-09-27 23:50:44 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+      child: Container(width: 50, height: 50, color: Colors.blue),
+    )
+        //home: Image.asset("image.png")
+        //home: Icon(Icons.star),
+        //home: Text("안녕"),
+        );
+  }
+}
+
+/*
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Title"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Drawr Header Part"),
+            ),
+            ListTile(
+              title: Text("Menu 1"),
+            )
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text("Hello World"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("clicked"),
+        child: const Icon(Icons.mouse),
+      ),
+    );
+  }
+}
+*/
